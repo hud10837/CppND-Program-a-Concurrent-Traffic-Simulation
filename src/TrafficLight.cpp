6f9c526f556679b53d7f6244f0da73542e689659
hud10837 <hud10837@esri.com>
2020-07-16 11:03:44 +0100

    fp.1

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -46,6 +46,8 @@ void TrafficLight::simulate()
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
 }
 
+*/
+
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
@@ -54,5 +56,3 @@ void TrafficLight::cycleThroughPhases()
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
 }
-
-*/
